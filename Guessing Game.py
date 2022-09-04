3a7eb2ced36e9fb89194e42b15dc482747028ace
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2022-09-04 10:34:34 -0500

    Create Guessing Game.py

---
+++ Guessing Game.py
@@ -0,0 +1,66 @@
+gameHasStarted = False
+wantsToContinue = True
+guessedNumberIsInt = True
+numberHasBeenGuessed = False
+
+randomNumber = random.randint(0, 1000)
+
+print(randomNumber)
+input("Welcome to the Number Guessing Game, press ENTER to start the game   ")
+
+while wantsToContinue == True:
+    
+    guessedNumber = input("Guess a number between 0 and 1000   ")
+    
+    try:
+        guessedNumberInt = int(guessedNumber)
+    except:
+        guessedNumberIsInt = False
+        while guessedNumberIsInt == False:
+            try:
+                guessedNumberInt = int(guessedNumber)
+            except:
+                guessedNumber = input("That's not a number, try again   ")
+            else:
+                guessedNumberIsInt = True
+                break
+            
+    while numberHasBeenGuessed != randomNumber:
+        if guessedNumberInt < randomNumber:
+            guessedNumber = input("That number was too low, try guessing a higher number   ")
+            try:
+                guessedNumberInt = int(guessedNumber)
+            except:
+                guessedNumberIsInt = False
+                while guessedNumberIsInt == False:
+                    try:
+                        guessedNumberInt = int(guessedNumber)
+                    except:
+                        guessedNumber = input("That's not a number, try again   ")
+                    else:
+                        guessedNumberIsInt = True
+                        break
+        elif guessedNumberInt > randomNumber:
+            guessedNumber = input("That number was too high, try guessing a lower number   ")
+            try:
+                guessedNumberInt = int(guessedNumber)
+            except:
+                guessedNumberIsInt = False
+                while guessedNumberIsInt == False:
+                    try:
+                        guessedNumberInt = int(guessedNumber)
+                    except:
+                        guessedNumber = input("That's not a number, try again   ")
+                    else:
+                        guessedNumberIsInt = True
+                        break
+        elif guessedNumberInt == randomNumber:
+            quit = input("Wow you guessed the number! Type QUIT to quit or press ENTER to play again   ")
+            
+            if quit == "QUIT":
+                wantsToContinue = False
+                break
+            else:
+                wantsToContinue = True
+                randomNumber = random.randint(0, 1000)
+                break
